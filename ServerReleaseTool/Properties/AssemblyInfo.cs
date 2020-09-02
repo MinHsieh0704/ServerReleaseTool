c675a962a22abd91179d440b06b2621c27ee6ec5
Min.Hsieh <a1991704@yahoo.com.tw>
2020-09-02 15:52:01 +0800

    v1.01.01

--- ServerReleaseTool/Properties/AssemblyInfo.cs
+++ ServerReleaseTool/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("ServerReleaseTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ServerReleaseTool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Min.Hsieh")]
 [assembly: AssemblyProduct("ServerReleaseTool")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，或將組建編號或修訂編號設為預設值
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
